b61594be9a6025a1a406842ba01c80a328e085d7
tuanzijiang <982675088@qq.com>
2018-10-08 11:05:18 +0800

    fix: ofer-cli不能获取配置信息和模板信息的bug
    1. 将config.js从main.js的逻辑中解耦出来
    2. 在template中引入缺少的包
    
    Change-Id: I9f95c1b03546729e5a2d6d41b19863ca3c48e606

--- .ofer/config.js
+++ .ofer/config.js
@@ -1,73 +1,71 @@
 const path = require('path');
 const fs = require('fs');
 
-
-
-const dataTypeFiles = fs.readdirSync(path.resolve(__rootname, './.ofer/data/dataType'));
-const dataObjs = dataTypeFiles.map(dataType => require(path.resolve(__rootname, `./.ofer/data/dataType/${dataType}`)));
-
-const { DATA_TYPE_ENUM, DATA_TYPE_ENUM_ENTITIES } = dataObjs.reduce((prev, curr) => {
-  return {
-    DATA_TYPE_ENUM: {
-      ...prev.DATA_TYPE_ENUM,
-      [curr.name]: curr.name,
-    },
-    DATA_TYPE_ENUM_ENTITIES: {
-      ...prev.DATA_TYPE_ENUM_ENTITIES,
-      [curr.name]: curr,
-    }
-  };
-}, {
-    DATA_TYPE_ENUM: {},
-    DATA_TYPE_ENUM_ENTITIES: {}
-  });
-
-const PATHS = {
-  CODE_PATH: path.resolve(__rootname, './code'),
-  TOOL_PATH: path.resolve(__rootname, './.ofer/tool.js'),
-  FILE_HANDLER_PATH: path.resolve(__rootname, './.ofer/IO/fileHandler'),
-  DATA_HANDLER_PATH: path.resolve(__rootname, './.ofer/data/dataHandler'),
-  OUTPUT_HANDLER_PATH: path.resolve(__rootname, './.ofer/IO/outputHandler'),
-  TOOL_HANDLER_PATH: path.resolve(__rootname, './.ofer/tool/toolHandler')
-}
-
-const platform = ['leetcode', 'nowcoder', 'acmcoder'];
-const platformEntities = {
-  leetcode: {
-    'zh-CN': '领扣网',
-    url: 'https://leetcode.com/',
-    defaultType: 'DEFAULT',
-    fnNameIsNeed: true
-  },
-  nowcoder: {
-    'zh-CN': '牛客网',
-    url: 'https://www.nowcoder.com',
-    defaultType: 'READ',
-    fnNameIsNeed: false
-  },
-  acmcoder: {
-    'zh-CN': '赛码网',
-    url: 'http://www.acmcoder.com/index',
-    defaultType: 'READ',
-    fnNameIsNeed: false
-  }
-}
-
-const originConfig = {
-  OUTPUT_PATH: './output.js',
-  TOOL_PATHS: [
-    '.ofer/toolHandler/tool.js'
-  ],
-  DATA_TYPE_ENUM,
-  DATA_TYPE_ENUM_ENTITIES,
-  platform,
-  platformEntities,
-  ...PATHS
-}
-
 const getConfig = (rootname) => {
+  const dataTypeFiles = fs.readdirSync(path.resolve(rootname, './.ofer/data/dataType'));
+  const dataObjs = dataTypeFiles.map(dataType => require(path.resolve(rootname, `./.ofer/data/dataType/${dataType}`)));
+
+  const { DATA_TYPE_ENUM, DATA_TYPE_ENUM_ENTITIES } = dataObjs.reduce((prev, curr) => {
+    return {
+      DATA_TYPE_ENUM: {
+        ...prev.DATA_TYPE_ENUM,
+        [curr.name]: curr.name,
+      },
+      DATA_TYPE_ENUM_ENTITIES: {
+        ...prev.DATA_TYPE_ENUM_ENTITIES,
+        [curr.name]: curr,
+      }
+    };
+  }, {
+      DATA_TYPE_ENUM: {},
+      DATA_TYPE_ENUM_ENTITIES: {}
+    });
+
+  const PATHS = {
+    CODE_PATH: path.resolve(rootname, './code'),
+    TOOL_PATH: path.resolve(rootname, './.ofer/tool.js'),
+    FILE_HANDLER_PATH: path.resolve(rootname, './.ofer/IO/fileHandler'),
+    DATA_HANDLER_PATH: path.resolve(rootname, './.ofer/data/dataHandler'),
+    OUTPUT_HANDLER_PATH: path.resolve(rootname, './.ofer/IO/outputHandler'),
+    TOOL_HANDLER_PATH: path.resolve(rootname, './.ofer/tool/toolHandler')
+  }
+
+  const platform = ['leetcode', 'nowcoder', 'acmcoder'];
+  const platformEntities = {
+    leetcode: {
+      'zh-CN': '领扣网',
+      url: 'https://leetcode.com/',
+      defaultType: 'DEFAULT',
+      fnNameIsNeed: true
+    },
+    nowcoder: {
+      'zh-CN': '牛客网',
+      url: 'https://www.nowcoder.com',
+      defaultType: 'READ',
+      fnNameIsNeed: false
+    },
+    acmcoder: {
+      'zh-CN': '赛码网',
+      url: 'http://www.acmcoder.com/index',
+      defaultType: 'READ',
+      fnNameIsNeed: false
+    }
+  }
+
+  const originConfig = {
+    OUTPUT_PATH: './output.js',
+    TOOL_PATHS: [
+      '.ofer/toolHandler/tool.js'
+    ],
+    DATA_TYPE_ENUM,
+    DATA_TYPE_ENUM_ENTITIES,
+    platform,
+    platformEntities,
+    ...PATHS
+  }
+
   const injectConfig = fs.existsSync(path.resolve(rootname, './config.json')) ?
-    require('../config') :
+    require(path.resolve(rootname, './config.json')) :
     {};
   return !injectConfig ? originConfig :
     Object.entries(injectConfig).reduce((prev, [configKey, configItem]) => {
